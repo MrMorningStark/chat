4db5bbd659b40e7cb0b646b87fb969cbf91b3260
Rajat Khatri <khatrirajat888@gmail.com>
2024-11-19 17:43:46 +0530

    used hosted backend

--- chat_frontend/src/services/api.js
+++ chat_frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.112:5000/api'
+    baseURL: 'https://chat-of3s.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
