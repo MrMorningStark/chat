cfca7a62eaa03536b7af73a2224330764f5ca22e
Rajat Khatri <khatrirajat888@gmail.com>
2024-11-20 17:51:33 +0530

    Revert "fixed some routing issue where on reloading it show page not found"
    This reverts commit b3191432914b268c5de22497f1d5846092ab124f.

--- chat_frontend/src/App.jsx
+++ chat_frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { unstable_HistoryRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
@@ -7,13 +7,12 @@ import Login from './components/Login';
 import ChatRoom from './components/ChatRoom';
 import WelcomeScreen from './components/WelcomeScreen';
 import Home from './components/Home';
-import { createBrowserHistory } from 'history';
+
 const App = () => {
-  const history = createBrowserHistory();
   return (
     <Provider store={store}>
       <ChakraProvider theme={theme}>
-        <Router history={history}>
+        <Router>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route
